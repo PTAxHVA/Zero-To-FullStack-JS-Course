eb0c83d78988eabdbcfad3bce6391fc2a02723a7
PTAxHVA <hoangtuananh26072007@gmail.com>
2025-11-13 17:33:27 +0700

    DOM 4

--- 03. DOM/Starter/4. Classlist/app.js
+++ 03. DOM/Starter/4. Classlist/app.js
@@ -1,4 +1,12 @@
 // classlist
+const h1 = document.querySelector("h1");
+console.log(h1.classList);
+
 // add()
+// h1.classList.add("styles");
+
 // remove()
+// h1.classList.remove("styles");
+
 // toggle()
+h1.classList.toggle("styles");
